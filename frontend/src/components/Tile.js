8abd90c52ebfd5e48b6cbbd613a66074363d2d37
Eric Groom <bcgroom@gmail.com>
2018-11-03 18:35:34 -0700

    temp

---
+++ frontend/src/components/Tile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Background = styled.div`
+    background-color: #222;
+    color: #fff;
+    font-weight: bold;
+    font-size: 2rem;
+    font-family: sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    p {
+        margin: none;
+        padding: none;
+        user-select: none;
+    }
+`
+
+const Tile = props => (
+    <Background onMouseDown={props.onMouseDown} onMouseUp={props.onMouseUp}>
+        <p>{props.letter}</p>
+    </Background>
+)
+
+export default Tile;
