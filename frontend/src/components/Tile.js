6baeb35e6410e439f53175e512da7669d44b9501
Eric Groom <egroom@mail.sfsu.edu>
2018-11-03 19:39:20 -0700

    build words with mouse events

--- frontend/src/components/Tile.js
+++ frontend/src/components/Tile.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Background = styled.div`
-    background-color: #222;
-    color: #fff;
-    font-weight: bold;
-    font-size: 2rem;
-    font-family: sans-serif;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  background-color: #222;
+  color: #fff;
+  font-weight: bold;
+  font-size: 2rem;
+  font-family: sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    p {
-        margin: none;
-        padding: none;
-        user-select: none;
-    }
-`
+  p {
+    margin: none;
+    padding: none;
+    user-select: none;
+  }
+`;
 
 const Tile = props => (
-    <Background onMouseDown={props.onMouseDown} onMouseUp={props.onMouseUp}>
-        <p>{props.letter}</p>
-    </Background>
-)
+  <Background {...props}>
+    <p>{props.letter}</p>
+  </Background>
+);
 
 export default Tile;
--- frontend/src/components/WordGrid.js
+++ frontend/src/components/WordGrid.js
@@ -1,38 +1,72 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import Tile from './Tile'
-
+import React, { Component } from "react";
+import _ from "lodash";
+import styled from "styled-components";
+import Tile from "./Tile";
 
 const Grid = styled.ul`
-    display: grid;
-    grid-template-columns: repeat(4, 4rem);
-    grid-template-rows: repeat(4, 4rem);
-    row-gap: 1rem;
-    column-gap: 1rem;
+  display: grid;
+  grid-template-columns: repeat(4, 4rem);
+  grid-template-rows: repeat(4, 4rem);
+  row-gap: 1rem;
+  column-gap: 1rem;
+  padding: 0;
+`;
+
+const CurrentWord = styled.h2`
+  text-align: center;
 `;
 
 export default class WordGrid extends Component {
+  state = {
+    currentIndex: null,
+    currentPath: null
+  };
   handleMouseDown(index) {
     console.log(`mouseDown ${index}`);
+    this.setState({ currentPath: [index] });
   }
   handleMouseUp(index) {
     console.log(`mouseUp ${index}`);
+    console.log(this.state.currentPath);
+    this.setState({ currentPath: null });
+  }
+  handleMouseEnter(index) {
+    if (this.state.currentPath) {
+      this.setState(({ currentPath }) => ({
+        currentIndex: index,
+        currentPath: [..._.takeWhile(currentPath, i => i !== index), index]
+      }));
+    } else {
+      this.setState({ currentIndex: index });
+    }
+  }
+  handleMouseLeave() {
+    this.setState({ currentIndex: null });
+  }
+  currentWord() {
+    if (this.state.currentPath === null) {
+      return "";
+    }
+    const letters = this.state.currentPath.map(i => this.props.letters[i]);
+    return letters.join("");
   }
   render() {
     return (
       <div>
         <Grid>
-        { this.props.letters.map((letter, index) => (
-            
-              <Tile 
-              letter={letter} 
-              onMouseDown={() => this.handleMouseDown(index)} 
-              onMouseUp={() => this.handleMouseUp(index)}
-              key={`${index}${letter}`} />
-            
-        ))}
+          {this.props.letters.map((letter, index) => (
+            <Tile
+              letter={letter}
+              onMouseDown={this.handleMouseDown.bind(this, index)}
+              onMouseUp={this.handleMouseUp.bind(this, index)}
+              onMouseEnter={this.handleMouseEnter.bind(this, index)}
+              onMouseLeave={this.handleMouseLeave.bind(this)}
+              key={index}
+            />
+          ))}
         </Grid>
+        <CurrentWord>{this.currentWord()}</CurrentWord>
       </div>
-    )
+    );
   }
 }
