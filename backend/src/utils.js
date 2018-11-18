09a2a5855557cbaf7471e2ff48af9ccfbcb0b64e
Eric Groom <bcgroom@gmail.com>
2018-11-18 15:02:39 -0800

    remove database related code

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const { createDbConnection, closeDbConnection } = require("./db");
 
 function readWords() {
   return new Promise((resolve, reject) => {
@@ -10,69 +9,6 @@ function readWords() {
   });
 }
 
-function groupWordsByFirstLetter(words) {
-  return words.reduce((acc, elem) => {
-    if (elem) {
-      if (acc[elem[0]]) acc[elem[0]].push(elem);
-      else acc[elem[0]] = [elem];
-    }
-    return acc;
-  }, {});
-}
-
-function keyedObjectToArray(obj) {
-  let arr = [];
-  Object.keys(obj).forEach(key => {
-    arr.push({ [key]: obj[key] });
-  });
-  return arr;
-}
-
-function populateDatabase() {
-  readWords()
-    .then(words => {
-      console.log(typeof words);
-      const wordMap = groupWordsByFirstLetter(words);
-      const documents = keyedObjectToArray(wordMap);
-      createDbConnection()
-        .then(db => {
-          db.collection("valid_words")
-            .insertMany(documents)
-            .then(data => {
-              console.log(`inserted ${data.result.n} documents`);
-              closeDbConnection();
-            })
-            .catch(err => {
-              console.error(err);
-              closeDbConnection();
-            });
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    })
-    .catch(err => {
-      console.error(err);
-    });
-}
-
-async function wordExists(word) {
-  if (!word) return false;
-  const query = { [word[0]]: word };
-  console.log(query);
-  const db = await createDbConnection();
-  const count = await db
-    .collection("valid_words")
-    .find(query)
-    .count();
-  closeDbConnection();
-  return count > 0;
-}
-
-wordExists("selfish").then(data => {
-  console.log(data);
-});
-
 module.exports = {
-  wordExists
+  readWords
 };
