a936e6c0fd2628add905fc4883a99a80229ef535
Eric Groom <bcgroom@gmail.com>
2018-11-17 17:48:16 -0800

    formatting

--- frontend/src/__tests__/utils.test.js
+++ frontend/src/__tests__/utils.test.js
@@ -1,104 +1,107 @@
 import {
-    appendOrRevert,
-    XYToIndex,
-    indexToXY,
-    bfs,
-    gridNeighbors
+  appendOrRevert,
+  XYToIndex,
+  indexToXY,
+  bfs,
+  gridNeighbors
 } from "../utils";
 
 describe("appendOrRevert", () => {
-    it("should append to an empty array", () => {
-        const result = appendOrRevert([], 1);
-        expect(result.length).toBe(1);
-        expect(result[0]).toBe(1)
-    });
-    it("should not modify original array", () => {
-        const original = [1, 2, 3];
-        const appended = appendOrRevert(original, 4);
-        expect(original).not.toBe(appended);
-        expect(original).toEqual([1, 2, 3]);
-        expect(appended).toEqual([1, 2, 3, 4]);
-    });
-    it("should revert when the element already exists in array", () => {
-        const result = appendOrRevert([5, 1, 2, 4], 1);
-        expect(result.length).toBe(2);
-        expect(result).toEqual([5, 1]);
-    });
-})
+  it("should append to an empty array", () => {
+    const result = appendOrRevert([], 1);
+    expect(result.length).toBe(1);
+    expect(result[0]).toBe(1);
+  });
+  it("should not modify original array", () => {
+    const original = [1, 2, 3];
+    const appended = appendOrRevert(original, 4);
+    expect(original).not.toBe(appended);
+    expect(original).toEqual([1, 2, 3]);
+    expect(appended).toEqual([1, 2, 3, 4]);
+  });
+  it("should revert when the element already exists in array", () => {
+    const result = appendOrRevert([5, 1, 2, 4], 1);
+    expect(result.length).toBe(2);
+    expect(result).toEqual([5, 1]);
+  });
+});
 
 describe("XYToIndex", () => {
-    it("works for happy path", () => {
-        const coord = {
-            x: 1,
-            y: 1
-        };
-        const index = XYToIndex(coord, 3);
-        expect(index).toBe(4);
-    })
-})
+  it("works for happy path", () => {
+    const coord = {
+      x: 1,
+      y: 1
+    };
+    const index = XYToIndex(coord, 3);
+    expect(index).toBe(4);
+  });
+});
 
 describe("IndexToXY", () => {
-    it("works for happy path", () => {
-        const index = 5;
-        const {
-            x,
-            y
-        } = indexToXY(index, 4);
-        expect(x).toBe(1);
-        expect(y).toBe(1);
-    })
-})
+  it("works for happy path", () => {
+    const index = 5;
+    const { x, y } = indexToXY(index, 4);
+    expect(x).toBe(1);
+    expect(y).toBe(1);
+  });
+});
 
 describe("IndexToXY <-> XYToIndex", () => {
-    it("should be reversible", () => {
-        const index = 7;
-        const gridSize = 2;
-        const coord = indexToXY(index, gridSize);
-        const newIndex = XYToIndex(coord, gridSize);
-        expect(index).toBe(newIndex);
-    })
-})
+  it("should be reversible", () => {
+    const index = 7;
+    const gridSize = 2;
+    const coord = indexToXY(index, gridSize);
+    const newIndex = XYToIndex(coord, gridSize);
+    expect(index).toBe(newIndex);
+  });
+});
 
 describe("grid neighbors", () => {
-    it("shouldn't include negative numbers", () => {
-        const neighbors = gridNeighbors(3)(2);
-        const negativeCount = neighbors.filter(i => i < 0).length;
-        expect(negativeCount).toBe(0);
-    })
-    it("shouldn't include numbers outside the range of the grid", () => {
-        const neighbors = gridNeighbors(2)(1);
-        // order is not guaranteed which is why we sort
-        expect(neighbors.sort((a, b) => a - b)).toEqual([0, 2, 3])
-    })
-    it("shouldn't contain the original index", () => {
-        const neighbors = gridNeighbors(2)(1);
-        expect(neighbors.includes(1)).toBe(false);
-    })
-})
+  it("shouldn't include negative numbers", () => {
+    const neighbors = gridNeighbors(3)(2);
+    const negativeCount = neighbors.filter(i => i < 0).length;
+    expect(negativeCount).toBe(0);
+  });
+  it("shouldn't include numbers outside the range of the grid", () => {
+    const neighbors = gridNeighbors(2)(1);
+    // order is not guaranteed which is why we sort
+    expect(neighbors.sort((a, b) => a - b)).toEqual([0, 2, 3]);
+  });
+  it("shouldn't contain the original index", () => {
+    const neighbors = gridNeighbors(2)(1);
+    expect(neighbors.includes(1)).toBe(false);
+  });
+});
 
 describe("bfs", () => {
-    it("should return an optimal path", () => {
-        const path = bfs(0, 2, gridNeighbors(3));
-        expect(path).toEqual([0, 1, 2]);
-    })
-    it("should return just two elements if they are neighbors", () => {
-        const path = bfs(0, 1, gridNeighbors(3));
-        expect(path).toEqual([0, 1]);
-    })
-    it("should support diagonals", () => {
-        const path = bfs(0, 3, gridNeighbors(2));
-        expect(path).toEqual([0, 3]);
-    })
-    it("should find shortest path along straights", () => {
-        const fromIndex = XYToIndex({
-            x: 0,
-            y: 0
-        }, 10);
-        const toIndex = XYToIndex({
-            x: 0,
-            y: 9
-        }, 10);
-        const path = bfs(fromIndex, toIndex, gridNeighbors(10));
-        expect(path.length).toBe(10);
-    })
-})
+  it("should return an optimal path", () => {
+    const path = bfs(0, 2, gridNeighbors(3));
+    expect(path).toEqual([0, 1, 2]);
+  });
+  it("should return just two elements if they are neighbors", () => {
+    const path = bfs(0, 1, gridNeighbors(3));
+    expect(path).toEqual([0, 1]);
+  });
+  it("should support diagonals", () => {
+    const path = bfs(0, 3, gridNeighbors(2));
+    expect(path).toEqual([0, 3]);
+  });
+  it("should find shortest path along straights", () => {
+    const fromIndex = XYToIndex(
+      {
+        x: 0,
+        y: 0
+      },
+      10
+    );
+    const toIndex = XYToIndex(
+      {
+        x: 0,
+        y: 9
+      },
+      10
+    );
+    const path = bfs(fromIndex, toIndex, gridNeighbors(10));
+    expect(path.length).toBe(10);
+  });
+});
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,5 @@
 import _ from "lodash";
 
-
 /**
  * If the element passed to this function already exists in the array,
  * the return value will be the array upto and including the element;
@@ -18,7 +17,7 @@ import _ from "lodash";
  * @returns {[T]}
  */
 export function appendOrRevert(array, elem) {
-    return [..._.takeWhile(array, e => e !== elem), elem];
+  return [..._.takeWhile(array, e => e !== elem), elem];
 }
 
 /**
@@ -28,10 +27,10 @@ export function appendOrRevert(array, elem) {
  * @returns {Object} coord
  */
 export function indexToXY(index, size) {
-    return {
-        x: index % size,
-        y: Math.floor(index / size)
-    };
+  return {
+    x: index % size,
+    y: Math.floor(index / size)
+  };
 }
 
 /**
@@ -41,7 +40,7 @@ export function indexToXY(index, size) {
  * @returns {number} index
  */
 export function XYToIndex(coord, size) {
-    return coord.y * size + coord.x;
+  return coord.y * size + coord.x;
 }
 
 /**
@@ -51,33 +50,39 @@ export function XYToIndex(coord, size) {
  * @returns function that takes index
  */
 export function gridNeighbors(size) {
-    return function (index) {
-        const {
-            x,
-            y
-        } = indexToXY(index, size);
-        let neighbors = [];
-        for (let dx = -1; dx <= 1; dx += 1) {
-            for (let dy = -1; dy <= 1; dy += 1) {
-                const newX = x + dx;
-                const newY = y + dy;
-                if (newX >= 0 && newX < size && newY >= 0 && newY < size && (newX !== x || newY !== y)) {
-                    const index = XYToIndex({
-                        x: newX,
-                        y: newY
-                    }, size);
-                    // trick to prefer straight lines over diagonal lines
-                    // iff the two have equal distances
-                    if (dx === 0 || dy === 0) {
-                        neighbors.unshift(index)
-                    } else {
-                        neighbors.push(index);
-                    }
-                }
-            }
+  return function(index) {
+    const { x, y } = indexToXY(index, size);
+    let neighbors = [];
+    for (let dx = -1; dx <= 1; dx += 1) {
+      for (let dy = -1; dy <= 1; dy += 1) {
+        const newX = x + dx;
+        const newY = y + dy;
+        if (
+          newX >= 0 &&
+          newX < size &&
+          newY >= 0 &&
+          newY < size &&
+          (newX !== x || newY !== y)
+        ) {
+          const index = XYToIndex(
+            {
+              x: newX,
+              y: newY
+            },
+            size
+          );
+          // trick to prefer straight lines over diagonal lines
+          // iff the two have equal distances
+          if (dx === 0 || dy === 0) {
+            neighbors.unshift(index);
+          } else {
+            neighbors.push(index);
+          }
         }
-        return neighbors;
+      }
     }
+    return neighbors;
+  };
 }
 
 /**
@@ -88,45 +93,44 @@ export function gridNeighbors(size) {
  * @returns {Array<number>} returns an array of path taken
  */
 export function bfs(from, to, neighborsFor) {
-    let queue = [from];
-    let visited = [from];
-    let parent = {};
-    const MAX_ITERATIONS = 2000;
+  let queue = [from];
+  let visited = [from];
+  let parent = {};
+  const MAX_ITERATIONS = 2000;
 
-    function backtrace(from, to, parent) {
-        let path = [to];
-        const MAX_LEN = 17;
-        let i = 0;
-        while (i < MAX_LEN && parent.hasOwnProperty(path[path.length - 1])) {
-            const prev = parent[path[path.length - 1]];
-            path.push(prev);
-            if (prev === from) {
-                return path;
-            }
-            i++;
-        }
-        if (i >= MAX_LEN)
-            throw new Error("backtrace max iterations exceeded");
-        return path;
-    }
+  function backtrace(from, to, parent) {
+    let path = [to];
+    const MAX_LEN = 17;
     let i = 0;
-    while (queue.length > 0 && i < MAX_ITERATIONS) {
-        const curr = queue.shift();
-        for (let n of neighborsFor(curr)) {
-            if (!visited.includes(n)) {
-                queue.push(n);
-                visited.push(n);
-                parent[n] = curr;
-            }
-            if (n === to) {
-                const path = backtrace(from, to, parent).reverse();
-                return path;
-            }
-        }
-        i++;
+    while (i < MAX_LEN && parent.hasOwnProperty(path[path.length - 1])) {
+      const prev = parent[path[path.length - 1]];
+      path.push(prev);
+      if (prev === from) {
+        return path;
+      }
+      i++;
     }
-    if (i >= MAX_ITERATIONS) {
-        throw new Error("bfs MAX_ITERATIONS surpassed");
+    if (i >= MAX_LEN) throw new Error("backtrace max iterations exceeded");
+    return path;
+  }
+  let i = 0;
+  while (queue.length > 0 && i < MAX_ITERATIONS) {
+    const curr = queue.shift();
+    for (let n of neighborsFor(curr)) {
+      if (!visited.includes(n)) {
+        queue.push(n);
+        visited.push(n);
+        parent[n] = curr;
+      }
+      if (n === to) {
+        const path = backtrace(from, to, parent).reverse();
+        return path;
+      }
     }
-    throw new Error("bfs path not found")
+    i++;
+  }
+  if (i >= MAX_ITERATIONS) {
+    throw new Error("bfs MAX_ITERATIONS surpassed");
+  }
+  throw new Error("bfs path not found");
 }
