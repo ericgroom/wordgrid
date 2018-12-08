45a201e7817b41b3849b643f4c82e158a0b82370
Eric Groom <bcgroom@gmail.com>
2018-12-08 11:04:11 -0800

    fix socketio path issue

--- frontend/src/sagas/index.js
+++ frontend/src/sagas/index.js
@@ -24,8 +24,8 @@ export function* putFrom(socketChannel) {
  */
 export default function* socketSaga() {
   while (true) {
-    const gameSocket = io(`${process.env.REACT_APP_BACKEND_URL}/game`);
-    const chatSocket = io(`${process.env.REACT_APP_BACKEND_URL}/chat`);
+    const gameSocket = io(`${process.env.REACT_APP_BACKEND_HOST}/game`, {path: process.env.REACT_APP_SOCKET_PATH});
+    const chatSocket = io(`${process.env.REACT_APP_BACKEND_HOST}/chat`, {path: process.env.REACT_APP_SOCKET_PATH});
     yield all([
       messagesFlow(chatSocket),
       gameFlow(gameSocket),
