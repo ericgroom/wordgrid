363fc2aaf91f7229d47ea52dab94db99e46a38d8
Eric Groom <egroom@mail.sfsu.edu>
2019-01-12 10:10:51 -0800

    add test path to <ChatWindow />

--- frontend/src/components/chat/__tests__/ChatWindow.test.js
+++ frontend/src/components/chat/__tests__/ChatWindow.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import ChatWindow from "../ChatWindow";
 
 describe("<ChatWindow />", () => {
@@ -17,5 +17,14 @@ describe("<ChatWindow />", () => {
       <ChatWindow sendMessage={() => {}} messages={messages} />
     );
     expect(wrapper).toMatchSnapshot();
+  });
+  it("can send a message", () => {
+    const onSend = jest.fn();
+    const wrapper = mount(<ChatWindow sendMessage={onSend} />);
+    wrapper
+      .find("form")
+      .first()
+      .simulate("submit");
+    expect(onSend.mock.calls.length).toBe(1);
   });
 });
