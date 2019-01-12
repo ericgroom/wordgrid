077d92f52d0d501b98908723a9b01d21caa89ad0
Eric Groom <egroom@mail.sfsu.edu>
2019-01-12 09:57:21 -0800

    add action creator tests

--- frontend/src/actions/__tests__/messages.test.js
+++ frontend/src/actions/__tests__/messages.test.js
@@ -2,8 +2,6 @@ import { receivedMessage, RECEIVED_MESSAGE } from "../messages";
 
 describe("messages actions", () => {
   it("receivedMessage", () => {
-    const message = "hello";
-    const action = receivedMessage(message);
-    expect(action).toEqual({ type: RECEIVED_MESSAGE, message });
+    expect(receivedMessage("hello")).toMatchSnapshot();
   });
 });
