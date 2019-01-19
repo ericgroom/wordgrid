388b537d5ec7cd348e4dc0aab4210a15a5aa00e2
Eric Groom <egroom@mail.sfsu.edu>
2019-01-19 12:52:42 -0800

    readd socket origins

--- backend/src/socketServer.js
+++ backend/src/socketServer.js
@@ -2,7 +2,9 @@ const io = require("socket.io");
 const eventHandlers = require("./events");
 
 exports.createServer = function(httpServer) {
-  return io(httpServer);
+  return io(httpServer, {
+    origins: "wordgrid.app:*"
+  });
 };
 
 exports.attachListeners = function(io, trie) {
